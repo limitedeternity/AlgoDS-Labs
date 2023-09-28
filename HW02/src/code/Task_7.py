d60b7d196f5b5f8ce413f0d1f1a792266423116b
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2023-09-28 21:36:47 +0300

    HW02/Task 7

--- HW02/src/code/Task_7.py
+++ HW02/src/code/Task_7.py
@@ -1,48 +1,25 @@
 from collections import deque
+from itertools import islice
 
+
+def window_gen(sequence, size):
+    assert(size > 0)
+
+    iterator = iter(sequence)
+    window = deque(islice(iterator, size), maxlen=size)
+
+    yield window
+
+    for elem in iterator:
+        window.append(elem)
+        yield window
+
+def max_window_sum(sequence, k):
+    return max(map(sum, window_gen(sequence, k)))
 
 def max_sub_sum(sequence, l, r):
-    assert(r - l >= 0)
-    assert(len(sequence) - l >= 0)
+    return max(max_window_sum(sequence, k) for k in range(l, r + 1))
 
-    current_sum = best_sum = 0
-    current_window = deque()
 
-    for x in sequence:
-        if len(current_window) == r:
-            elem = current_window.popleft()
-            current_sum -= elem
-            best_sum = max(current_sum, best_sum)
- 
-            temp_window = deque()
-            print(current_window, current_sum, best_sum)
-
-            while len(current_window) > l:
-                elem = current_window.pop()
-                current_sum -= elem
-                best_sum = max(current_sum, best_sum)
-            
-                temp_window.appendleft(elem)
-                print(current_window, current_sum, best_sum)
-
-            while len(temp_window) > 0:
-                elem = temp_window.popleft()
-                current_sum += elem
-                best_sum = max(current_sum, best_sum)
-                
-                current_window.append(elem)
-                print(current_window, current_sum, best_sum)
-            
-        current_window.append(x)
-        current_sum += x
-
-        if l <= len(current_window) <= r:
-            best_sum = max(current_sum, best_sum)
-        
-        print(current_window, current_sum, best_sum)
-        print()
-
-    return best_sum
-
-    
 assert(max_sub_sum([1, 2, -5, 3, 2, -1, 5, -10, 3, 2], 2, 4) == 9)
+assert(max_sub_sum([-1, -2, -5, -3, 10], 2, 5) == 7)
