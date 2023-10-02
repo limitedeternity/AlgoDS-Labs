0ac2f0036a4b515bb56ad02536fd2319eb54a4b8
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2023-10-02 16:48:32 +0300

    Fix HW02/Task 4

--- HW02/src/code/Task_4.py
+++ HW02/src/code/Task_4.py
@@ -1,29 +1,32 @@
 from collections import deque
 
 
-def longest_balanced(string):
+def longest_valid(string):
     brackets = { "{": "}", "(": ")", "[": "]" }
     stack = deque()
+    stack.append((None, -1))
 
-    cur_length = max_length = 0
+    max_length = 0
 
-    for char in string:
+    for i, char in enumerate(string):
         if char in brackets:
-            stack.append(char)
-            cur_length += 1
+            stack.append((char, i))
 
         else:
-            if len(stack) == 0 or brackets[stack.pop()] != char:
-                cur_length = 0
+            open_br, _ = stack.pop()
+
+            if brackets.get(open_br, open_br) != char:
+                stack.append((None, i))
                 continue
-                
-        if len(stack) == 0:
-            max_length = max(max_length, cur_length * 2)
+
+            _, last_i = stack[-1]
+            max_length = max(max_length, i - last_i)
 
     return max_length
 
 
-assert(longest_balanced("))([]({})))(())") == 8)
-assert(longest_balanced(")(([]({})))([]") == 10)
-assert(longest_balanced("()(())((()") == 6)
-assert(longest_balanced(")(([](})))[]") == 2)
+assert(longest_valid("))([]({})))(())") == 8)
+assert(longest_valid(")(([]({})))([]") == 10)
+assert(longest_valid("()(())((()") == 6)
+assert(longest_valid(")(([](})))[]") == 2)
+assert(longest_valid("(}[]") == 2)
